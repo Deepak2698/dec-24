fb100f79de7fe567f6f849faed2018d8ac59105d
Deepak Mishra <38439630+Deepak2698@users.noreply.github.com>
2025-03-07 21:58:31 +0530

    dec-24

---
+++ src/view/Functional.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Functional(){
+    return(
+        <div>
+            <h2>
+                This is Functional Component
+            </h2>
+        </div>
+    )
+}
+
+export default Functional;
